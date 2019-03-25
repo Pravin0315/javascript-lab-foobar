fc13c1dbcf382a5c11d7c2d544a03bced87ec1d0
Pravin Prasad <pravin0315@gmail.com>
2019-03-25 15:25:22 -0400

    adding files to git

---
+++ class.js
@@ -0,0 +1,18 @@
+var p1
+var p2
+var score1
+var score2
+
+
+var weapons = ["rock", "paper", "scissors"];
+
+var weaponOfChoice = weapons[Math.floor(Math.random() * weapons.length)];
+
+while ([score1<3]||[score2<3]){
+    
+}
+
+
+
+
+
---
+++ class2.js
@@ -0,0 +1,16 @@
+let myvar= 1;
+
+myvar="c";
+
+switch (myvar){
+    case "a":
+        console.log("Hey");
+        break;
+    case "b":
+        console.log("Foo");
+        console.log("Bar");
+        break;
+    case "c":
+        console.log("Bar");
+        break;
+}
